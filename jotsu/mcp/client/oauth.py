d8844d3576184f43508d02d480e7fffd2d4b0463
Matthew R. Laue <mattlaue@gmail.com>
2025-06-25 11:21:26 -0700

    Allow a none client_secret for OAuth2AuthorizationCodeClient

--- jotsu/mcp/client/oauth.py
+++ jotsu/mcp/client/oauth.py
@@ -49,7 +49,8 @@ class OAuth2AuthorizationCodeClient:
     """Client for the OAuth2.1 flow required by MCP."""
 
     def __init__(
-            self, *, authorize_endpoint: str, token_endpoint: str, scope: str, client_id: str, client_secret: str,
+            self, *,
+            authorize_endpoint: str, token_endpoint: str, scope: str, client_id: str, client_secret: str | None = None,
             **_kwargs  # ignored
     ):
         self.authorize_endpoint = authorize_endpoint
